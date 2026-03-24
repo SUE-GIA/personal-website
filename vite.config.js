3ace8196dc9c65ddbec93f2ef69bf40703a0d3e9
StefanosGiannakos <sgiannakosp@gmail.com>
2026-03-24 02:26:12 +0200

    domain

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/personal-website/',
+  base: '/',
   plugins: [
     vue(),
     tailwindcss(),
